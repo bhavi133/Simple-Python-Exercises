08c2328457802d87b74e575682fad8e5962e9b56
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2022-02-01 12:36:43 +0530

    Create Problem 19.py

---
+++ Problem 19.py
@@ -0,0 +1,10 @@
+Statement : "99 Bottles of Beer" is a traditional song in the United States and Canada. It is popular to sing on long trips, as it has a very repetitive format which is easy to memorize, and can take a long time to sing. The song's simple lyrics are as follows: 99 bottles of beer on the wall, 99 bottles of beer. Take one down, pass it around, 98 bottles of beer on the wall. The same verse is repeated, each time with one fewer bottle. The song is completed when the singer or singers reach zero. Your task here is write a Python program capable of generating all the verses of the song
+
+def sing_99_bottles_of_beer():
+    n = 99
+    song = ""
+    for i in range(n, 0, -1):
+        song = song + f"{i} bottles of beer on the wall, {i} bottles of beer. Take one down, pass it around, {i-1} bottles of beer on the wall\n"
+    return song
+
+print(sing_99_bottles_of_beer())
